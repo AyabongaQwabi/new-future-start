1e5161a33881bc2b806ddf6c234eda1e50c45e54
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-08-04 10:41:51 +0200

    add google anaLYTICS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { Toaster } from "sonner"
+import type React from 'react';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'sonner';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Future Start - Conquering Your Years in Tertiary Education",
+  title: 'Future Start - Conquering Your Years in Tertiary Education',
   description:
-    "Your comprehensive guide to navigating university life successfully. Avoid common pitfalls and graduate with excellence.",
-    generator: 'v0.dev'
-}
+    'Your comprehensive guide to navigating university life successfully. Avoid common pitfalls and graduate with excellence.',
+  generator: 'v0.dev',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         {children}
-        <Toaster position="bottom-right" />
+        <Toaster position='bottom-right' />
+        <GoogleAnalytics gaId='G-JL1XZPJG3V' />
       </body>
     </html>
-  )
+  );
 }
